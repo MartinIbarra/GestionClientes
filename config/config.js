646b0feee1a77f74e453e2788266ea64080a795c
MartinIbarra <oso.caballito@hotmail.com>
2015-05-27 16:48:08 -0300

    arreglando nombre de carpeta y proyecto

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ var config = {
   development: {
     root: rootPath,
     app: {
-      name: 'node-v2b'
+      name: 'node-manager'
     },
     port: 3000,
     db: 'mongodb://localhost/localDB'
@@ -16,7 +16,7 @@ var config = {
   test: {
     root: rootPath,
     app: {
-      name: 'node-v2b'
+      name: 'node-manager'
     },
     port: 3000,
     db: 'mongodb://localhost/test'
@@ -25,7 +25,7 @@ var config = {
   production: {
     root: rootPath,
     app: {
-      name: 'node-v2b'
+      name: 'node-manager'
     },
     port: 3000,
     db: 'mongodb://localhost/production'
